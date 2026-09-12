2257129168e445cd071286bed9df10877aefcdde
Tonmoy Islam <293394739+tonmoyislam-deventest@users.noreply.github.com>
2026-09-12 21:25:40 +0600

    refactor: hero section's height

--- src/assets/components/hero/Hero.tsx
+++ src/assets/components/hero/Hero.tsx
@@ -4,7 +4,7 @@ import HeroImg from "./HeroImg";
 const Hero = () => {
   return (
     // Hero
-    <section className="h-[calc(h-100vh-81px)] w-full">
+    <section className="h-[calc(100vh-81px)] w-full">
       <div className="mx-auto grid container gap-10 grid-cols-2 items-center px-10 py-20">
 
         {/* Left Side */}
