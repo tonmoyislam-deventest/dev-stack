015a5569500203ddd154b4159cfbb80664d2d811
Tonmoy Islam <293394739+tonmoyislam-deventest@users.noreply.github.com>
2026-09-13 03:46:23 +0600

    Feat: make navbar sticky

--- src/components/navbar/Navar.tsx
+++ src/components/navbar/Navar.tsx
@@ -4,7 +4,7 @@ import SignIn from "../navbar/SignIn";
 import SignUp from "../navbar/SignUp";
 const Navbar = () => {
   return (
-    <nav className="w-full border-b border-gray-100 ">
+    <nav className="w-full border-b border-gray-100 sticky top-0 backdrop-blur-3xl">
       <div className="mx-auto flex h-20 container items-center justify-between px-10">
         <Logo />
         <Navigation />
