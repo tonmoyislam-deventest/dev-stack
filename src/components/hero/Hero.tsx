074dbec2a53342b2f09aac41b84ae18d34bb8eae
Tonmoy Islam <293394739+tonmoyislam-deventest@users.noreply.github.com>
2026-09-13 13:57:34 +0600

    Update: Hero Components Size

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -4,8 +4,8 @@ import HeroImg from "./HeroImg";
 const Hero = () => {
   return (
     // Hero
-    <section className="h-[calc(100vh-81px)] w-full">
-      <div className="mx-auto grid container gap-10 grid-cols-2 items-center px-10 py-20">
+    <section className="w-full">
+      <div className="mx-auto grid container gap-10 grid-cols-2 items-center px-10">
 
         {/* Left Side */}
         <HeroContent/>        
